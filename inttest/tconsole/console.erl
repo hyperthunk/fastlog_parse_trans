46e2d0da73ecd7145835b4a47b10ad8e8ddee6fe
Tim Watson <watson.timothy@gmail.com>
2011-06-13 15:53:13 +0100

    Update to static deps, fix clean inttest

--- inttest/tconsole/console.erl
+++ inttest/tconsole/console.erl
@@ -4,29 +4,29 @@
 -fastlog({parse_trans.verbose, true}).
 
 main() ->
-	fastlog:start(),
-	fastlog:add_logger(?MODULE, debug),
-	Logger = 'fastlog.special.logger',
-	fastlog:add_logger(Logger, debug),
-	hello("World"),
-	goodbye("foo"),
-	warn_number(33),
-	info(),
-	log(Logger),
-	ok.
+    fastlog:start(),
+    fastlog:add_logger(?MODULE, debug),
+    Logger = 'fastlog.special.logger',
+    fastlog:add_logger(Logger, debug),
+    hello("World"),
+    goodbye("foo"),
+    warn_number(33),
+    info(),
+    log(Logger),
+    ok.
 
 hello(World) ->
-	?DEBUG("Hello ~s~n", [World]),
-	ok.
+    ?DEBUG("Hello ~s~n", [World]),
+    ok.
 
 goodbye(Name) ->
-	?ERROR("Goodbye ~s~n", [Name]).
+    ?ERROR("Goodbye ~s~n", [Name]).
 
 warn_number(N) ->
-	?WARN("Warning #~p~n", [N]).
+    ?WARN("Warning #~p~n", [N]).
 
 info() ->
-	?INFO("INFO MSG ~p~n", [123]).
+    ?INFO("INFO MSG ~p~n", [123]).
 
 log(Logger) ->
-	fastlog:info(Logger, "Log ~s~n", ["this"]).
+    fastlog:info(Logger, "Log ~s~n", ["this"]).
--- inttest/tconsole/tconsole_rt.erl
+++ inttest/tconsole/tconsole_rt.erl
@@ -5,35 +5,37 @@
 -include_lib("hamcrest/include/hamcrest.hrl").
 
 files() ->
-	[{copy, "console.erl", "src/console.erl"},
-	 {create, "ebin/console_app.app", app(console_app, [console])}].
+    [{copy, "../../rebar", "rebar"},
+     {copy, "rebar.config", "rebar.config"},
+     {copy, "console.erl", "src/console.erl"},
+     {create, "ebin/console_app.app", app(console_app, [console])}].
 
 run(Dir) ->
-	retest_log:log(debug, "Dir: ~p~n", [Dir]),
-	?assertMatch({ok, _}, retest:sh("rebar get-deps compile -v")),
-	Ref = retest:sh("erl -pa ebin -s console main", [{async, true}]),
-	{ok, Hello} = retest:sh_expect(Ref,
-	                "\\[nonode@nohost\\] \\[<.*>\\] \\[warn\\] \\[console\\] "
-	                "\\[hello\\] \\[line:19\\] Hello World",
-	                [{capture, all, list}]),
+    retest_log:log(debug, "Dir: ~p~n", [Dir]),
+    ?assertMatch({ok, _}, retest:sh("./rebar get-deps compile-deps -v")),
+    Ref = retest:sh("erl -pa ebin -s console main", [{async, true}]),
+    {ok, Hello} = retest:sh_expect(Ref,
+                    "\\[nonode@nohost\\] \\[<.*>\\] \\[warn\\] \\[console\\] "
+                    "\\[hello\\] \\[line:19\\] Hello World",
+                    [{capture, all, list}]),
     retest_log:log(debug, "[CAPTURED]: ~s~n", [Hello]),
-	{ok, Goodbye} = retest:sh_expect(Ref,
-	                "\\[nonode@nohost\\] \\[<.*>\\] \\[error\\] \\[console\\] "
-	                "\\[goodbye\\] \\[line:23\\] Goodbye foo",
-	                [{capture, all, list}]),
+    {ok, Goodbye} = retest:sh_expect(Ref,
+                    "\\[nonode@nohost\\] \\[<.*>\\] \\[error\\] \\[console\\] "
+                    "\\[goodbye\\] \\[line:23\\] Goodbye foo",
+                    [{capture, all, list}]),
     retest_log:log(debug, "[CAPTURED]: ~s~n", [Goodbye]),
     {ok, Warning} = retest:sh_expect(Ref,
                     "\\[nonode@nohost\\] \\[<.*>\\] \\[warn\\] \\[console\\] "
                     "\\[warn_number\\] \\[line:26\\] Warning #33",
-	                [{capture, all, list}]),
-	retest_log:log(debug, "[CAPTURED]: ~s~n", [Warning]),
+                    [{capture, all, list}]),
+    retest_log:log(debug, "[CAPTURED]: ~s~n", [Warning]),
     {ok, Info} = retest:sh_expect(Ref,
-	                "\\[nonode@nohost\\] \\[<.*>\\] \\[info\\] \\[console\\] "
-	                "\\[info\\] \\[line:29\\] INFO MSG 123",
-	                [{capture, all, list}]),
-	retest_log:log(debug, "[CAPTURED]: ~s~n", [Info]),
+                    "\\[nonode@nohost\\] \\[<.*>\\] \\[info\\] \\[console\\] "
+                    "\\[info\\] \\[line:29\\] INFO MSG 123",
+                    [{capture, all, list}]),
+    retest_log:log(debug, "[CAPTURED]: ~s~n", [Info]),
     retest_sh:kill(Ref),
-	ok.
+    ok.
 
 %%
 %% Generate the contents of a simple .app file
